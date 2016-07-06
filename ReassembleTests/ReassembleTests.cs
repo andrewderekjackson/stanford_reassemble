cabf74d53e06f7c5e08d70e03cb9ae79c216f9e6
Andrew <andrewderekjackson@gmail.com>
2016-07-07 10:31:06 +1200

    Upgrading to .net 4.6.1 Adding failing test case - to implement later

--- ReassembleTests/ReassembleTests.cs
+++ ReassembleTests/ReassembleTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Reassemble;
 
@@ -98,6 +99,20 @@ namespace ReassembleTests {
             ReassembleImpl impl = new ReassembleImpl();
             Assert.AreEqual("all is well that ends well", impl.Reassemble(fragments));
         }
+
+        [TestMethod]
+        public void TestGitHubSample2() {
+            var fragments =
+                "m quaerat voluptatem.;pora incidunt ut labore et d;, consectetur, adipisci velit;olore magnam aliqua;idunt ut labore et dolore magn;uptatem.;i dolorem ipsum qu;iquam quaerat vol;psum quia dolor sit amet, consectetur, a;ia dolor sit amet, conse;squam est, qui do;Neque porro quisquam est, qu;aerat voluptatem.;m eius modi tem;Neque porro qui;, sed quia non numquam ei;lorem ipsum quia dolor sit amet;ctetur, adipisci velit, sed quia non numq;unt ut labore et dolore magnam aliquam qu;dipisci velit, sed quia non numqua;us modi tempora incid;Neque porro quisquam est, qui dolorem i;uam eius modi tem;pora inc;am al"
+                    .Split(';').Select(s => s.TrimStart()).ToArray();
+
+            var expectedResult = "Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.";
+
+            ReassembleImpl impl = new ReassembleImpl();
+            Assert.AreEqual(expectedResult, impl.Reassemble(fragments));
+
+        }
+
 
     }
 }
