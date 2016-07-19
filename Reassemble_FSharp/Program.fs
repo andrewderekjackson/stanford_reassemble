d9a7c1eb6ecd31d4eacf3be99bd275e2929dcd51
Andrew <andrewderekjackson@gmail.com>
2016-07-19 15:34:34 +1200

    Adding F# project and WIP

---
+++ Reassemble_FSharp/Program.fs
@@ -0,0 +1,29 @@
+﻿
+open System;
+
+open Reassemble
+
+[<EntryPoint>]
+let main argv = 
+    
+    let step1 = find_overlap "ell that en" "hat end"
+    if step1 <> 6 then
+        failwith "Step 1: Expected 6"
+
+    let step1_text = merge "ell that en" "hat end"
+
+
+
+    let step2 = find_overlap "ell that end" "t ends well"
+    if step2 <> 5 then
+        failwith "Step 2: Expected 5"
+    
+    printfn "%i" step2
+        
+    
+    System.Console.ReadLine() |> ignore
+    0
+
+
+
+
---
+++ Reassemble_FSharp/Reassemble.fs
@@ -0,0 +1,21 @@
+﻿module Reassemble
+
+let reduce_left (s:string) = s.Substring(1,s.Length-1)
+let reduce_right (s:string) = s.Substring(0, s.Length-1)
+
+let merge (s1:string) (s2:string) (index:int) =
+    s1 + s2
+
+let rec find_overlap_core (s1:string) (s2:string) (reduce: (string) -> (string)) = 
+    
+    match s1 with
+        | _ when s1 = "" || s2 = "" -> 0
+        | _ when s1.Contains(s2) -> s2.Length
+        | _ -> find_overlap_core s1 (reduce s2) reduce
+               
+
+let rec find_overlap (s1:string) (s2:string) =
+    max (find_overlap_core s1 s2 reduce_left) (find_overlap_core s1 s2 reduce_right)
+    
+
+ 
